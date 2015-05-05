d83946dbb8fd76fcab4e2ea6df49ebe2d102d672
jkprice07 <jkprice07@gmail.com>
2015-05-05 12:30:43 +0100

    Update map_client_node.py

--- src/map_client_node.py
+++ src/map_client_node.py
@@ -18,6 +18,9 @@ RATE = 2
 
 class MapClientNode:
 
+    ######################################################
+    #  Initialise client with server address HOST_ADDR and 
+    #  map file directory MAP_DIR.
     def __init__(self, HOST_ADDR, MAP_DIR):
         self.HOST_ADDR = HOST_ADDR
         self.MAP_DIR = MAP_DIR
@@ -28,39 +31,49 @@ class MapClientNode:
         #if(os.path.isfile(self.MAP_DIR + '/map.yaml')):
         #    os.remove(self.MAP_DIR + '/map.yaml')
         self.MAP_FLAG = None
-        # Subscriptions
-        rospy.Subscriber('/map_bot_base/state', String, self.MapStateCallback)
-        rospy.Subscriber('/slam_out_pose', PoseStamped, self.MapPoseCallback)
-        # Publications
+        #  Subscriptions for MAP state and MAP pose.
+        rospy.Subscriber('/map_bot_base/state',
+                         String,
+                         self.MapStateCallback)
+        rospy.Subscriber('/slam_out_pose',
+                         PoseStamped,
+                         self.MapPoseCallback)
+        #  Publication for server state.
         self.SERV_STATE = rospy.Publisher('/client_node/serv_state', String)
 
+    #######################################
+    #  Updates MAP state in client.
     def MapStateCallback(self, DATA):
         self.MAP_CLIENT.SetState(DATA.data)
 
+    ###############################################
+    #  Updates MAP pose in client.  Converts pose
+    #  to a dictionary, converts the data to euler
+    #  co-ords from quaternion and send to server.
     def MapPoseCallback(self, DATA):
         POSE_DICT = PoseStampedToDict(DATA)
-        QUAT = [POSE_DICT['pose']['orientation']['x'],
-                POSE_DICT['pose']['orientation']['y'],
-                POSE_DICT['pose']['orientation']['z'],
-                POSE_DICT['pose']['orientation']['w']]
-        EUL = euler_from_quaternion(QUAT)
-        POSE_DICT = QuaternionToEulerDict(POSE_DICT, EUL)
-        self.MAP_CLIENT.SetPose(str(POSE_DICT))
+        POSE_DICT_STRING = str(QuaternionToEulerDict(POSE_DICT)
+        self.MAP_CLIENT.SetPose(POSE_DICT_STRING)
 
+    ####################################################
+    #  Work callback, executed at RATE (defined at top).
     def WorkCallback(self):
+        #  Publish current server state.
         CUR_SERV_STATE = self.MAP_CLIENT.ServState()
         self.SERV_STATE.publish(CUR_SERV_STATE)
-
+        #  Display current server state (if updated).
         if(self.PRINT_FLAG != CUR_SERV_STATE):
             self.PRINT_FLAG = CUR_SERV_STATE
             rospy.loginfo(self.PRINT_FLAG)
-
+        #  When server state is MAP_DONE, the mapping is stopped,
+        #  the map is saved, the `.yaml' file is amended and both
+        #  the `.pgm' and modified `.yaml' are sent to the server.
         if(CUR_SERV_STATE == 'MAP_DONE'):
             if(not self.MAP_FLAG):
                 subprocess.call('cd')
                 subprocess.call('rosrun map_server \
                     map_saver -f map',  shell=True)
-                time.sleep(5)
+                time.sleep(3)
                 YAML_DATA = self.ReadFile(self.MAP_DIR + '/map.yaml')
                 YAML_DATA = self.FixYAML(YAML_DATA)
                 self.MAP_CLIENT.SendFile(YAML_DATA, 'MAP_YAML')
@@ -70,7 +83,9 @@ class MapClientNode:
         else:
             self.MAP_FLAG = os.path.isfile(self.MAP_DIR + '/map.pgm') and \
                 os.path.isfile(self.MAP_DIR + '/map.yaml')
-
+                
+    #######################################################
+    #  Amends first line of `.yaml' DATA to fix image name.
     def FixYAML(self, DATA):
         if(DATA):
             i = 0
@@ -83,6 +98,8 @@ class MapClientNode:
         else:
             return None
 
+    ##################################################
+    #  Reads binary data from file specified by PATH.
     def ReadFile(self, PATH):
         try:
             FILE = open(PATH, 'rb')
@@ -92,6 +109,9 @@ class MapClientNode:
         except:
             return None
 
+    ###############################################
+    #  Spin function starts/stops MAP client and 
+    #  executes Work Callback at desired RATE.
     def Spin(self):
         self.MAP_CLIENT.Start()
         rate = rospy.Rate(RATE)
@@ -103,8 +123,6 @@ class MapClientNode:
 if __name__ == "__main__":
     rospy.init_node('map_client_node')
     IP, MAP_DIR = ReadSettings('MAPBOT')
-    #IP = '192.168.0.117'
-    #MAP_DIR = '/home/smartlab-tb01/catkin_ws_hydro/src/robot_comm'
     PORT = int(raw_input('Enter port:\n'))
     HOST_ADDR = (IP, PORT)
     CLIENT_NODE = MapClientNode(HOST_ADDR, MAP_DIR)
