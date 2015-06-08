3097b91f6b9e9d36fc336eeb1af32aabdf6d622f
jkprice07 <jkprice07@gmail.com>
2015-06-08 14:44:46 +0100

    Update read_settings.py

--- src/read_settings.py
+++ src/read_settings.py
@@ -8,11 +8,11 @@ def Decode(LINE):
 
 def ReadSettings(TYPE):
     #FILE = open()
-    if(os.path.isfile(os.getcwd() + '/settings.txt', 'rb')):
+    if(os.path.isfile(os.getcwd() + '/settings.txt')):
         FILE = open(os.getcwd() + '/settings.txt', 'rb')
-    elif(os.path.isfile(os.getcwd() + '/src/settings.txt', 'rb')):
+    elif(os.path.isfile(os.getcwd() + '/src/settings.txt')):
         FILE = open(os.getcwd() + '/src/settings.txt', 'rb')
-    elif(os.path.isfile(os.path.split(os.path.abspath(os.getcwd()))[0] + '/settings.txt', 'rb')):
+    elif(os.path.isfile(os.path.split(os.path.abspath(os.getcwd()))[0] + '/settings.txt')):
         FILE = open(os.path.split(os.path.abspath(os.getcwd()))[0] + '/settings.txt', 'rb')
     else:
         return 0
