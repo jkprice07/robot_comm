b9f0da476e72659130b95ddce51f249876475f40
jkprice07 <jkprice07@gmail.com>
2015-06-09 13:45:23 +0100

    Update ipad_client.py

--- src/ipad_client.py
+++ src/ipad_client.py
@@ -18,7 +18,7 @@ class Ipad:
         SERV_STATE = self.CLIENT.ServState()
         if(SERV_STATE == 'USER_DEC'):
             self.CLIENT.Send(None , 32, 'INPUT#PICKUP#')
-        elif(SERV_STATE == 'PICKUP_CHECK):
+        elif(SERV_STATE == 'PICKUP_CHECK'):
             self.CLIENT.Send(None , 32, 'INPUT#SUCC#')
 
     def Spin(self):
