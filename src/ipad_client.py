32baee411d38160d07217f30e5ace4e2e1607f07
jkprice07 <jkprice07@gmail.com>
2015-06-09 13:41:14 +0100

    Update ipad_client.py

--- src/ipad_client.py
+++ src/ipad_client.py
@@ -1,82 +1,49 @@
 #!/usr/bin/python
 
-# Version: 1.1
+# Version: 1.0
 
-import rospy
 import os
+import threading
+import time
 from read_settings import *
 from clientclass import BotClient
-from move_base_msgs.msg import *
-from geometry_msgs.msg import PoseStamped
-from pose_dict_tf import *
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
-
-RATE = 10
 
 
-class FlyNode:
+class Ipad:
 
-    ######################################################
-    #  Initialise client with server address HOST_ADDR and
-    #  map file directory MAP_DIR.
-    def __init__(self, HOST_ADDR, MAP_DIR):
-        self.FLY_CLIENT = BotClient(HOST_ADDR, 'FLYBOT')
-        self.MAP_DIR = MAP_DIR
-        self.PRINT_FLAG = None
-        if(os.path.isfile(self.MAP_DIR + '/map.pgm')):
-            os.remove(self.MAP_DIR + '/map.pgm')
-        if(os.path.isfile(self.MAP_DIR + '/map.yaml')):
-            os.remove(self.MAP_DIR + '/map.yaml')
-        self.MAP_FLAG = None
-        self.RVIZ_POSE = rospy.Subscriber('/move_base_simple/goal',
-                                          PoseStamped, self.PoseCallback)
+    def __init__(self, HOST_ADDR):
+        self.CLIENT = BotClient(HOST_ADDR, '')
 
-    ####################################################
-    #  Uploads object pose to server.
-    def PoseCallback(self, DATA):
-        POSE_DICT = PoseStampedToDict(DATA)
-        self.FLY_CLIENT.SendObjPose(str(POSE_DICT))
-
-    ####################################################
-    #  Work callback, executed at RATE (defined at top).
     def WorkCallback(self):
-        CUR_SERV_STATE = self.FLY_CLIENT.ServState()
-        #  Display current server state (if updated)
-        if(self.PRINT_FLAG != CUR_SERV_STATE):
-            self.PRINT_FLAG = CUR_SERV_STATE
-            rospy.loginfo(self.PRINT_FLAG)
-        #  If no map present, check server state for MAP_AT_SERVER
-        #  and download to MAP_DIR if it's available from server.
-        if(CUR_SERV_STATE == 'MAP_AT_SERVER'):
-            self.MAP_FLAG = os.path.isfile(self.MAP_DIR + '/map.pgm') and \
-                os.path.isfile(self.MAP_DIR + '/map.yaml')
-            if(not self.MAP_FLAG):
-                self.FLY_CLIENT.RecvMap(self.MAP_DIR)
-                self.MAP_FLAG = True
-        #  Reset map flag variable, removing existing
-        #  files on reset.
-        elif(CUR_SERV_STATE == 'RESET'):
-            if(os.path.isfile(self.MAP_DIR + '/map.pgm')):
-                os.remove(self.MAP_DIR + '/map.pgm')
-            if(os.path.isfile(self.MAP_DIR + '/map.yaml')):
-                os.remove(self.MAP_DIR + '/map.yaml')
-            self.MAP_FLAG = None
+        SERV_STATE = self.CLIENT.ServState()
+        if(SERV_STATE = 'USER_DEC'):
+            self.CLIENT.Send(, 32, 'INPUT#PICKUP#')
+        elif(SERV_STATE = 'PICKUP_CHECK):
+            self.CLIENT.Send(, 32, 'INPUT#SUCC#')
 
-    ####################################################
-    #  Spin function starts/stops FLY client and 
-    #  executes Work Callback at desired RATE.
     def Spin(self):
-        rate = rospy.Rate(RATE)
-        self.FLY_CLIENT.Start()
-        while not rospy.is_shutdown():
+        self.CLIENT.Start()
+        while(True):
+            print '1 to START,\n\
+                2 to signal MAP completion,\n\
+                3 to RESET,\n\
+                4 to QUIT.'
+            ACTION = raw_input('')
+            if(ACTION == '1'):
+                self.CLIENT.Send(, 32, 'START#')
+            elif(ACTION == '2'):
+                self.CLIENT.Send(, 32, 'INPUT#TRUE#')
+            elif(ACTION == '3'):
+                sself.CLIENT.Send(, 32, 'RESET#')
+            elif(ACTION == '4'):
+                break
             self.WorkCallback()
-            rate.sleep()
-        self.FLY_CLIENT.Stop()
+            time.sleep(1)
+        self.CLIENT.Stop()
 
 if __name__ == '__main__':
-    rospy.init_node('fly_sim_node')
-    IP, MAP_DIR = ReadSettings('FLYBOT')
+    IP, _ = ReadSettings('FLYBOT')
     PORT = int(raw_input('Enter port:\n'))
     HOST_ADDR = (IP, PORT)
-    FLY = FlyNode(HOST_ADDR, MAP_DIR)
-    FLY.Spin()
+    CLIENT = Ipad(HOST_ADDR)
+    CLIENT.Spin()
